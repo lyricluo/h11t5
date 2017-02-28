34ba4e95b685d819d633f50838da8333a74d29b3
linziyanleo <linziyanleo@outlook.com>
2017-02-28 00:13:09 -0800

    All done

--- h11t52.cpp
+++ h11t52.cpp
@@ -4,11 +4,8 @@ using namespace std;
 struct Node{
     int data;
     Node *link;
-};Node * head;
-
+};
 typedef struct Node Node;
-
-
 
 struct LinkedList{
 	Node *head;
@@ -16,16 +13,29 @@ struct LinkedList{
 };
 typedef struct LinkedList LinkedList;
 
-void creatList(){
-    head = new Node;
-}
 
 void add(struct Node *head, int value) {
-    Node *p = new Node;
-    p->data = value;
-    p->link = head->link;
-    head->link = p;
-    
+         // if list is NULL, we can do nothing.
+
+         Node *p = new Node;
+         //p=new Node;// temporary pointer
+
+         p->data = value;// Set p's data field to the value passed in
+                 
+        // if (head == NULL){
+          //   head=p;
+            // p->link=NULL;      
+             // Make both head and tail of this list point to p
+             // Set p's next field to NULL
+
+
+         // }else{
+          p->link = head;
+          head = p;
+         // }
+          // If Add p at the end of the list
+                      // Set p's next field point to the previous head of the list
+                      // Set the head of list point to p
                      
 }
 
@@ -71,29 +81,29 @@ void print(struct Node *head){
 int main (){
     Node *head=NULL;
     head=new Node;
-    //head->data=6;
-    //head->link=new Node;
-    //head->link->data=6;
-    //head->link->link=new Node;
-    //head->link->link->data=1;
-    //head->link->link->link=new Node;
-    //head->link->link->link->data=3;
-    //head->link->link->link->link=new Node;
-    //head->link->link->link->link->data=7;
-    //head->link->link->link->link->link=new Node;
-    //head->link->link->link->link->link->data=6;
-    //head->link->link->link->link->link->link=NULL;
+    head->data=6;
+    head->link=new Node;
+    head->link->data=6;
+    head->link->link=new Node;
+    head->link->link->data=1;
+    head->link->link->link=new Node;
+    head->link->link->link->data=3;
+    head->link->link->link->link=new Node;
+    head->link->link->link->link->data=7;
+    head->link->link->link->link->link=new Node;
+    head->link->link->link->link->link->data=6;
+    head->link->link->link->link->link->link=NULL;
     int x=6;
     int y=7;
     int z=3;
     int a=5;
-    add(head, x);
-    add(head,a);
-    add(head,z);
-    add(head,x);
-    add(head,y);
-    add(head,x);
-    print(head);
+    //add(head, x);
+    //add(head,a);
+    //add(head,z);
+    //add(head,x);
+    //add(head,y);
+    //add(head,x);
+    //print(head);
     deleteNode (head, x);
     print(head);
     return 0;
